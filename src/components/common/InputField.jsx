02fff81498fe145e77441b6c2440a0f131ab9ca0
Leonardo Oliveira <leocienciasocial@gmail.com>
2025-10-28 00:02:06 -0300

    Produção do logo

--- src/components/common/InputField.jsx
+++ src/components/common/InputField.jsx
@@ -4,8 +4,8 @@ import "./InputField.css";
 function FormularioCriarConta() {
     return (
     <div className= "formulario_cadastro">  
-        nome = prompt('Digite seu nome completo');
-        email = prompt('Digite o seu e-mail');
+        nome = prompt('Digite seu nome completo'); Sei que não é assim. Vou alterar mais tarde,
+        email = prompt('Digite o seu e-mail');     quando eu entender mais de react :p
         senha = prompt('Digite sua senha');
     </div>  
     )
