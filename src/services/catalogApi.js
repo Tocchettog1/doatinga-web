0535f2e474dd3606b324f6045a181ce14502eea5
leobriao <leocienciasocial@gmail.com>
2025-12-01 23:31:40 -0300

    integrado front com back

--- src/services/catalogApi.js
+++ src/services/catalogApi.js
@@ -1,7 +1,8 @@
 // src/services/catalogApi.js
 export async function fetchCatalogItems() {
-  const response = await fetch('/items.json');
+  const response = await fetch('http://localhost:3333/api/institutions');
   if (!response.ok) throw new Error('Erro ao carregar catálogo');
-  const data = await response.json();
-  return data;
+  
+  const json = await response.json();
+  return json.data;
 }
