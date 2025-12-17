ad712c58534f200ae713303757173994d0ad0063
Leonardo <leoandrighetto02@gmail.com>
2025-12-16 21:40:39 -0300

    @fix Desabilitar botão de cadastro quando o formulário ainda não foi preenchido, falta fazer os retornos visuais

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,10 +1,11 @@
 
-function Button({ children, onClick, className, type = "button" }) {
+function Button({ children, disabled, onClick, className, type = "button" }) {
     return (
         <button
             type = {type}
             className = {className}
             onClick = {onClick}
+            disabled={disabled}
         >
             {children}
         </button>
